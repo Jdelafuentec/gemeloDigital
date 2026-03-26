5dc049ff17616161265b6ffffa6f7f75ee2cdf61
Jdelafuentec <javidelafountain@gmail.com>
2026-03-26 18:14:20 -0300

    Convert FMU folder to standard ZIP archive and add Linux dependencies

--- pump_v3_basicCold_t_linux.fmu/sources/pump_v3_basicCold_t_11mix.c
+++
@@ -1,9 +0,0 @@
-/* Mixed Systems */
-#include "pump_v3_basicCold_t_model.h"
-#include "pump_v3_basicCold_t_11mix.h"
-/* initial mixed systems */
-/* initial_lambda0 mixed systems */
-/* parameter mixed systems */
-/* model mixed systems */
-/* jacobians mixed systems */
-
--- pump_v3_basicCold_t_linux.fmu/sources/pump_v3_basicCold_t_16dae.c
+++
@@ -1,9 +0,0 @@
-/* DAE residuals is empty */
-#include "pump_v3_basicCold_t_model.h"
-#ifdef __cplusplus
-extern "C" {
-#endif
-int pump_v3_basicCold_t_initializeDAEmodeData(DATA* data, DAEMODE_DATA* daeModeData){ return -1; }
-#ifdef __cplusplus
-}
-#endif
